b1d261ca3f0f55f75f30970e1bc98bd7ef9a5e1f
Chandan9030 <chandanpradhan414@gmail.com>
2025-11-01 10:32:08 +0530

    RU

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const Booking = require('./models/Booking');
 // Nodemailer configuration
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
